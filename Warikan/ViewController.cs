d0a91b0edb08936e8f361eb3bd8223f86abf0a8c
jusco0103 <rysk.myst.biz@gmail.om>
2018-02-02 14:49:51 +0900

    0.01

--- Warikan/ViewController.cs
+++ Warikan/ViewController.cs
@@ -6,6 +6,7 @@ namespace Warikan
 {
     public partial class ViewController : UIViewController
     {
+        
         protected ViewController(IntPtr handle) : base(handle)
         {
             // Note: this .ctor should not contain any initialization logic.
