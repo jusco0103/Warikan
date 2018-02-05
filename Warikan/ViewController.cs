c64ee06227faab57102b3a8241e35c5a078f0a46
jusco0103 <rysk.myst.biz@gmail.om>
2018-02-05 20:50:08 +0900

    0.02

--- Warikan/ViewController.cs
+++ Warikan/ViewController.cs
@@ -6,7 +6,61 @@ namespace Warikan
 {
     public partial class ViewController : UIViewController
     {
-        
+        partial void WarikanButton_TouchUpInside(UIButton sender)
+        {
+
+            //入力フォームが空欄だった場合のSystem.FormatException対策
+            if (KingakuForm.Text == "")
+            {
+                KingakuForm.Text = "0";
+            }
+            if (NinzuForm.Text == "")
+            {
+                NinzuForm.Text = "0";
+            }
+
+            //変数宣言
+            int Kingaku;
+            int Ninzu;
+
+            //入力フォームの値(.Text)をintに変換して格納
+            Kingaku = Convert.ToInt32(KingakuForm.Text);
+            Ninzu = Convert.ToInt32(NinzuForm.Text);
+
+            //金額が0円の場合はアラート表示
+            if (Kingaku == 0)
+            {
+                var ac = UIAlertController.Create("アラート", "合計金額又は、人数が未入力です。", UIAlertControllerStyle.Alert);
+                var action = UIAlertAction.Create("OK", UIAlertActionStyle.Default, null);
+                ac.AddAction(action);
+                PresentViewController(ac, true, null);
+            }
+
+            //人数が１人以下の場合はアラート表示
+            else if (Ninzu <= 1)
+            {
+                var ac = UIAlertController.Create("アラート", "人数は２名以上入力してください。", UIAlertControllerStyle.Alert);
+                var action = UIAlertAction.Create("OK", UIAlertActionStyle.Default, null);
+                ac.AddAction(action);
+                PresentViewController(ac, true, null);
+            }
+
+            //計算処理＆stringに変換して格納
+            else
+            {
+                double Kekka1 = Kingaku / Ninzu;
+                Kekka1 = Math.Ceiling(Kekka1 * 0.01f) * 100;
+                string Kekka2 = Kekka1.ToString();
+                //ラベルに表示
+                KekkaLabel.Text = Kekka2;
+            }
+         }
+
+        partial void ResetButton_TouchUpInside(UIButton sender)
+        {
+            KingakuForm.Text = "";
+            NinzuForm.Text = "";
+         }
         protected ViewController(IntPtr handle) : base(handle)
         {
             // Note: this .ctor should not contain any initialization logic.
--- Warikan/ViewController.designer.cs
+++ Warikan/ViewController.designer.cs
@@ -1,17 +1,72 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
 namespace Warikan
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel KekkaLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField KingakuForm { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField NinzuForm { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ResetButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton WarikanButton { get; set; }
+
+        [Action ("ResetButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void ResetButton_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("WarikanButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void WarikanButton_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
         {
+            if (KekkaLabel != null) {
+                KekkaLabel.Dispose ();
+                KekkaLabel = null;
+            }
+
+            if (KingakuForm != null) {
+                KingakuForm.Dispose ();
+                KingakuForm = null;
+            }
+
+            if (NinzuForm != null) {
+                NinzuForm.Dispose ();
+                NinzuForm = null;
+            }
+
+            if (ResetButton != null) {
+                ResetButton.Dispose ();
+                ResetButton = null;
+            }
+
+            if (WarikanButton != null) {
+                WarikanButton.Dispose ();
+                WarikanButton = null;
+            }
         }
     }
 }
